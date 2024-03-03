a886151e72b6732de15d5b624da6fdbf8d0f3f05
Alien Deployer <m@a17.name>
2024-03-03 22:56:14 +0300

    fix local deploy

--- deploy/202_LiquidityFactory.ts
+++ deploy/202_LiquidityFactory.ts
@@ -5,6 +5,7 @@ import { ethers } from 'hardhat';
 import { TokenFactory__factory, LiquidityFactory, LiquidityFactory__factory, IWeightedPoolFactory__factory, ERC20__factory, MockWeth9__factory, IBVault__factory, IBPT__factory } from '../typechain';
 import { AbiCoder, ZeroAddress, parseUnits, EventLog } from 'ethers'
 import {BalancerUtils} from "../scripts/utils/balancer-utils";
+import {DeployerUtils} from "../scripts/deploy/DeployerUtils";
 
 const NAME = 'LiquidityFactory';
 
@@ -18,30 +19,54 @@ const func: DeployFunction = async function(hre: HardhatRuntimeEnvironment) {
   let liquidityFactoryAddress = await getDeployedContractByName('LiquidityFactory')
   let bptAddress: string|undefined
   let lpFactory: LiquidityFactory|undefined
+  let wethAddress: string = ''
+  let balancerVault: string = ''
   if (!liquidityFactoryAddress) {
+    let args = []
+    if (hre.network.config.chainId == 31337) {
+      const balancerCore = await DeployerUtils.deployBalancer(signer);
+
+      const weightedPoolFactory = await DeployerUtils.deployContract(signer, 'WeightedPoolFactory', ...[
+        await balancerCore.vault.getAddress(),
+        await balancerCore.protocolFeePercentagesProvider.getAddress(),
+        BalancerUtils.PAUSE_WINDOW_DURATION,
+        BalancerUtils.BUFFER_PERIOD_DURATION
+      ])
+
+      const weth = await DeployerUtils.deployContract(signer, 'MockWeth9')
+      args = [await weightedPoolFactory.getAddress(), await weth.getAddress()]
+      wethAddress = await weth.getAddress()
+      balancerVault = await balancerCore.vault.getAddress()
+    } else {
+      wethAddress = WETH
+      args = [BALANCER_WEIGHTED_POOL_FACTORY, WETH]
+      balancerVault = BALANCER_VAULT
+    }
+
     const result = await deploy(NAME, {
       contract: 'LiquidityFactory',
       from: deployer,
       log: true,
       skipIfAlreadyDeployed: true,
       ...(await txParams(hre, ethers.provider)),
-      args: [BALANCER_WEIGHTED_POOL_FACTORY, WETH],
+      args,
     });
     liquidityFactoryAddress = result.address
     lpFactory = LiquidityFactory__factory.connect(liquidityFactoryAddress, signer)
     bptAddress = ZeroAddress
   } else {
     lpFactory = LiquidityFactory__factory.connect(liquidityFactoryAddress, signer)
-    bptAddress = await lpFactory.deployedBPT(signer.address)
+    bptAddress = await lpFactory.deployedBPT(deployer)
   }
 
   const tokenFactoryAddress = await getDeployedContractByName('TokenFactory')
   const tokenFactory = TokenFactory__factory.connect(tokenFactoryAddress, ethers.provider)
   const token = await tokenFactory.token()
 
+
   if (bptAddress == ZeroAddress) {
     await lpFactory.deployBPT(token)
-    bptAddress = await lpFactory.deployedBPT(signer.address)
+    bptAddress = await lpFactory.deployedBPT(deployer)
   }
 
   const isPoolInited = await BalancerUtils.isPoolInited(bptAddress, signer)
@@ -49,24 +74,24 @@ const func: DeployFunction = async function(hre: HardhatRuntimeEnvironment) {
   if (isPoolInited) {
     console.log('Pool already inited')
   } else {
-    let assets = [token,WETH]
+    let assets = [token,wethAddress as string]
     // start price = $1 ~= 0.0003, initial weth amount = 0.01 (1e16)
     let initialBalances = [parseUnits('0.01') * 3333n * 4n,parseUnits('0.01')]
     if (BigInt(assets[0]) > BigInt(assets[1])) {
       assets = [assets[1], assets[0]]
       initialBalances = [initialBalances[1], initialBalances[0]]
     }
-    const weth9 = MockWeth9__factory.connect(WETH, signer)
+    const weth9 = MockWeth9__factory.connect(wethAddress as string, signer)
     await weth9.deposit({value: parseUnits('0.01')})
-    await ERC20__factory.connect(token, signer).approve(BALANCER_VAULT, parseUnits('200'))
-    await weth9.approve(BALANCER_VAULT, parseUnits('0.01'))
+    await ERC20__factory.connect(token, signer).approve(balancerVault, parseUnits('200'))
+    await weth9.approve(balancerVault, parseUnits('0.01'))
 
-    const vault = IBVault__factory.connect(BALANCER_VAULT, signer)
+    const vault = IBVault__factory.connect(balancerVault, signer)
     const poolId = await IBPT__factory.connect(bptAddress as string, signer).getPoolId()
-    await vault.joinPool(
+    const tx = await vault.joinPool(
       poolId,
-      signer.address,
-      signer.address,
+      deployer,
+      deployer,
       {
         assets,
         maxAmountsIn: initialBalances,
@@ -77,6 +102,7 @@ const func: DeployFunction = async function(hre: HardhatRuntimeEnvironment) {
         fromInternalBalance: false,
       }
     )
+    await tx.wait()
   }
 
   /* Deploying pool manually
